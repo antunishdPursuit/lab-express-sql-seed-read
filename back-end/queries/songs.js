a96eb35797fb740a002008574cc0aa02a0903522
antunishdPursuit <dennysantunish@pursuit.org>
2023-07-20 01:07:31 -0400

    Fixed create route error

--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -24,7 +24,7 @@ const getSong = async (id) => {
 const createSong = async (song) => {
     try {
       const newSong = await db.one(
-        "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4) RETURNING *",
+        "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
         [song.name, song.artist, song.album, song.time, song.is_favorite]
       );
       return newSong;
