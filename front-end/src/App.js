cd3c1b4f3065c94d31a53988fa36ce64ba8dc1b0
antunishdPursuit <dennysantunish@pursuit.org>
2023-07-21 20:56:10 -0400

    added boostrap and routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,33 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Edit from "./Pages/Edit";
+import ErrorPage from "./Pages/ErrorPage";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
+
 function App() {
   return (
-    <div className="">
-      <h1>Tuner App</h1>
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/songs" element={<Index />} />
+            <Route path="/songs/new" element={<New />} />
+            <Route path="/songs/:index" element={<Show />} />
+            <Route path="/songs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
